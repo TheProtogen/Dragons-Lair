95b0f7fd0bab50fd9e4100596ef99c8771bae8c4
TheProtogen <luepez357@gmail.com>
2023-09-03 21:07:39 -0300

    -Teste com table, e algumas alterações usando CSS - Organização do código no javascript
    new file:   imagens/rainbow background.png
    
    modified:   index.html
    modified:   processador1.js
    modified:   style.css

--- processador1.js
+++ processador1.js
@@ -4,16 +4,28 @@
 //Então dá para se observar que alguns elementos perdem o atributo hidden, enquanto outros ganham o mesmo
 
 let toggle = button => {
-       let element0 = document.getElementById("myDiv1");
-       let element1 = document.getElementById("buttonThing")
-       let element2 = document.getElementById("myDiv2");
-       let element3 = document.getElementById("myHr");
-       let element4 = document.getElementById("myDiv3");
+       //Coisas para mostrar
+       let myDiv1 = document.getElementById("myDiv1");
+       let myDiv2 = document.getElementById("myDiv2");
+       let myDiv3 = document.getElementById("myDiv3");
 
-       element0.removeAttribute("hidden");
-       element1.setAttribute("hidden","hidden");
-       element2.removeAttribute("hidden");
-       element3.setAttribute("hidden","hidden");
-       element4.removeAttribute("hidden","hidden");
-}    
-    
+       let myTable1 = document.getElementById("myTable1");
+
+
+       //Coisas para esconder
+       let buttonThing = document.getElementById("buttonThing");
+       let myHr = document.getElementById("myHr");
+       
+
+       //Comando para mostrar
+       myDiv1.removeAttribute("hidden");
+       myDiv2.removeAttribute("hidden");
+       myDiv3.removeAttribute("hidden");
+
+       myTable1.removeAttribute("hidden")
+
+
+       //Comando para esconder
+       buttonThing.setAttribute("hidden","hidden");
+       myHr.setAttribute("hidden","hidden");
+}
